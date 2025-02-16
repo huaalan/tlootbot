c588bf2ba66fa19580a69a7bb5dee1e80709324c
huaalan <alanhua@live.com>
2025-02-16 00:25:26 -0500

    Simplify commands
    - removed channel setting option
    - selecting channel interaction updates to remove select menu

--- commands/slash-commands/post-settings-command.js
+++ commands/slash-commands/post-settings-command.js
@@ -7,36 +7,15 @@ const {
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('settings')
-		.setDescription('Settings to configure which channels to post loot.')
-        .addStringOption(option =>
-			option.setName('channel_type')
-				.setDescription('The gif category')
-				.setRequired(true)
-                .addChoices(
-                    { name: 'Upload Channel', value: 'upload_channel' },
-                    { name: 'Thread Channel', value: 'thread_channel' },
-                )
-    ),
+		.setDescription('Settings to configure which channels to post loot.'),
 	async execute(interaction) {
-        const choice = interaction.options.getString('channel_type');
         const row = new ActionRowBuilder();
-        let instr;
-
-        if ( choice === 'upload_channel' ) {
-            instr = 'Select channel where loot image is uploaded.';
-            const select_upload_channel = new ChannelSelectMenuBuilder()
-                .setCustomId('select_upload_channel')
-                .setPlaceholder('Select upload channel for loot')
-                .addChannelTypes('GuildText');
-            row.addComponents(select_upload_channel);
-        } else if ( choice === 'thread_channel' ) {
-            instr = 'Select channel for the bot to post loot.';
-            const select_thread_channel = new ChannelSelectMenuBuilder()
-                .setCustomId('select_thread_channel')
-                .setPlaceholder('Select channel to post loot')
-                .addChannelTypes('GuildForum');
-            row.addComponents(select_thread_channel);
-        }
+        let instr = 'Select channel for the bot to post loot.';
+        const select_thread_channel = new ChannelSelectMenuBuilder()
+            .setCustomId('select_thread_channel')
+            .setPlaceholder('Select channel to post loot')
+            .addChannelTypes('GuildForum');
+        row.addComponents(select_thread_channel);
 
 		await interaction.reply({
             content: instr,
--- events/interaction-selectmenu-event.js
+++ events/interaction-selectmenu-event.js
@@ -1,4 +1,4 @@
-const { Events, MessageFlags, ComponentType } = require('discord.js');
+const { Events, MessageFlags } = require('discord.js');
 const { QuickDB } = require("quick.db");
 
 const db = new QuickDB();
@@ -10,19 +10,11 @@ module.exports = {
 
         const guild_id = interaction.guild.id;
 
-        if (interaction.customId === 'select_upload_channel') {
-            await db.set(`guild_${guild_id}.upload_channel`, interaction.values[0])
-            .then(() => {
-                interaction.reply({ content: 'Channel saved.', flags: MessageFlags.Ephemeral });
-            })
-            .catch((err) => {
-                console.error(err);
-                interaction.reply({ content: 'An error occurred.', flags: MessageFlags.Ephemeral });
-            });
-        } else if (interaction.customId === 'select_thread_channel') {
+        if (interaction.customId === 'select_thread_channel') {
             await db.set(`guild_${guild_id}.thread_channel`, interaction.values[0])
             .then(() => {
-                interaction.reply({ content: 'Channel saved.', flags: MessageFlags.Ephemeral });
+                // interaction.reply({ content: 'Channel saved.', flags: MessageFlags.Ephemeral });
+                interaction.update({ content: 'Channel setting saved.', components: [] });
             })
             .catch((err) => {
                 console.error(err);
